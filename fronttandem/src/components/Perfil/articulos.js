1a7a99654f2d0163a22c5b0ab07732637c07f881
alfredommid <alfredommid@gmail.com>
2022-01-10 20:33:56 -0600

    feat:remove build folder, new card design

--- fronttandem/src/components/Perfil/articulos.js
+++ fronttandem/src/components/Perfil/articulos.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useContext, useEffect } from 'react';
 import ArticuloContext from '../../context/articulos/articuloContext';
 import AuthContext from '../../context/auth/authContext';
-import { Card, CardText, CardBody,CardTitle, CardSubtitle } from 'reactstrap';
+import { Card, CardText, CardBody,CardTitle, CardSubtitle, CardFooter } from 'reactstrap';
 
 const ArticulosPerfil = () => {
     const authContext = useContext(AuthContext);
@@ -20,46 +20,47 @@ const ArticulosPerfil = () => {
 
     return ( 
         <Fragment>
-            <div className="container row articard-cont">
+            <div className="container articard-cont">
                 {articulos
                     ?   articulos.articulos.map( item => 
-                        <Card key={item._id} id={item._id} className="col-5 m-3">
-                            <CardBody className="container">
-                                <div className="container mb-4 d-flex item-icontitle">
-                                    <CardTitle tag="h3">{item.modelo}</CardTitle>
-                                    <div className="container item-icons">
-                                        <button className="btn edit">
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-trash" width="22" height="22" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                                <line x1="4" y1="7" x2="20" y2="7" />
-                                                <line x1="10" y1="11" x2="10" y2="17" />
-                                                <line x1="14" y1="11" x2="14" y2="17" />
-                                                <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                                                <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                                            </svg>
-                                        </button>
-                                        <button className="btn edit">
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-edit" width="22" height="22" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                                <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
-                                                <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
-                                                <line x1="16" y1="5" x2="19" y2="8" />
-                                            </svg>
-                                        </button>
+                        <Card key={item._id} id={item._id} className="col-3 m-1 cont-card">
+                            <CardBody className="cont-cardbody">
+                                    <div className="cont-color">
+                                        <div className="container cont-img"></div>
                                     </div>
-                                    
-                                </div>
-                                <CardSubtitle tag="h6" className="mb-2 text-muted">{item.marca}</CardSubtitle>
-                                <CardText>Color: {item.color}</CardText>
-                                <CardText>Talla: {item.talla}</CardText>
-                                <CardText>Tipo: {item.tipoBicileta}</CardText>
-                                <CardText>{item.tipoEntrada}</CardText>
-                                <div className="container d-flex footer">
-                                    <CardText>Creado: {fechaRefactor(item.creado)}</CardText>
-                                    <CardText>Estatus: Pendiente</CardText>
-                                </div>
-                                
+                                    <div className="container cont-info">
+                                        <div className="container title-sub">
+                                            <p className="text-1">{item.modelo}</p>
+                                            <p  className="text-2">{item.marca}</p>
+                                        </div>
+                                        <div className="container specs">
+                                            <p className="text-3">Talla: {item.talla}</p>
+                                            <p className="text-3">{item.tipoBicicleta}</p>
+                                            <p className="text-3 estatus">Pendiente</p>
+                                        </div>
+                                    </div>
                             </CardBody>
+                            <CardFooter className="cont-cardfooter">
+                                    <p className="footer-text">{item.tipoEntrada}</p>
+                                    <button className="btn delete">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-trash" width="22" height="22" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                            <line x1="4" y1="7" x2="20" y2="7" />
+                                            <line x1="10" y1="11" x2="10" y2="17" />
+                                            <line x1="14" y1="11" x2="14" y2="17" />
+                                            <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+                                            <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+                                        </svg>
+                                    </button>
+                                    <button className="btn edit">
+                                        <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-edit" width="22" height="22" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                            <path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+                                            <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+                                            <line x1="16" y1="5" x2="19" y2="8" />
+                                        </svg>
+                                    </button>
+                            </CardFooter>
                         </Card>
                         )
                     :   null
--- fronttandem/src/index.js
+++ fronttandem/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +7,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
       <App />
-    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
